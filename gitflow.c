fc072ceb8d7defaee7621f141c1ec56b7ac6d175
Lucas <lucassilva@byronsolutions.com>
2026-05-14 09:15:00 -0300

    feature(#2): adicionada mais yma mágica

--- gitflow.c
+++ gitflow.c
@@ -3,11 +3,17 @@
 int main()
 {
     float n;
+    int idade;
     printf("Digite um número para eu adivinhar: ");
     scanf("%f",&n);
 
     printf("Esse é o seu número?: %.2d",n);
 
+    printf("Digite sua idade: ");
+    scanf("%d",idade);
+    printf("Some 2");
+    printf("Subtraia 2");
+    printf("Essa é a sua idade: %.2d"),idade;
 
     return 0;
 }
